d06cb1c2fda43d76efb0d682c41594bdfd4a52b2
Valeriya Kot <valeriia.ko@room4.team>
2021-10-24 09:51:17 +0300

    Modifyy footer

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@
   const $like = document.querySelector("#like");
   $like.addEventListener("click", () => ($like.style.color = "red"));
 })();
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const timeEl = document.getElementById("time");
+  const time = new Date().getFullYear();
+  timeEl.insertAdjacentHTML("afterbegin", time);
+});

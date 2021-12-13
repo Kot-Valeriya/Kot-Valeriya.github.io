7dacd9dbadf719335202c323770a9f85f4645d7d
Kot-Valeriya <kot.valeriia@gmail.com>
2021-12-13 22:22:31 +0200

    Change homepage

---
+++ dist/index.063d27e6.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"))}(),document.addEventListener("DOMContentLoaded",(e=>{const t=document.getElementById("time"),n=(new Date).getFullYear();t.insertAdjacentHTML("afterbegin",n)}));
+//# sourceMappingURL=index.063d27e6.js.map
